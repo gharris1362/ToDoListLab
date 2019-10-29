34a191f23b9931e960e5f616601959272c43c647
student <student@Admins-iMac.local>
2019-10-29 16:46:32 -0500

    almost done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,28 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import * as serviceWorker from './serviceWorker';
 let root = document.getElementById('root')
-let button = document.getElementById('addTask')
-let input = document.getElementById('input')
-let Tasklist = document.getElementById('tasks');
 
 
-// class App extends Component {
-//     render() {
-//         return (
-//             <React.Fragment>
-              
-//             </React.Fragment>
-//         )
-//     }
-// }
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <h1 id='head'> To-Do List</h1>
+                <div id='importantStuff'>
+                    <input placeholder="Enter Task Here" id="input"></input>
+                    <button id='addTask' onClick={() => {
+                        let ahh = document.getElementById('input');
+                        let ahh2 = document.getElementById('tasks');
+                        let ahh3 = document.createElement('br')
+                        let task = ahh.value
+                        if (task === '') { alert('No Task Entered') }
+                        else { ahh2.append(ahh.value, ahh3); ahh.value = '' }
+                    }}>Add</button>
+                </div>
+                <br />
+                <hr id='seperator' />
+                <div id='tasks'>
+                    <h1 id='tasksTitle'>Tasks</h1>
+                </div>
+            </React.Fragment>
+        )
+    }
 
 
-// ReactDOM.render(<App />, root)
+}
 
 
-button.addEventListener('click', () => {
-   Tasklist.append(input.value);
-})
+
+
+// const element = <App button={<button id='addTask'>Add</button>}/>
+
+
+
+
+
+ReactDOM.render(<App />, root)
 
 
 
